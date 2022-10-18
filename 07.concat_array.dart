5fec13df68ac320f855b83fb12e26b2c91b9e46b
Bayazid Hasan <78470516+hbayazid969@users.noreply.github.com>
2022-10-19 01:04:16 +0600

    Create 07.concat_array.dart

---
+++ 07.concat_array.dart
@@ -0,0 +1,11 @@
+//leetcode
+//https://leetcode.com/problems/concatenation-of-array/
+//1929. Concatenation of Array
+
+class Solution {
+  List<int> getConcatenation(List<int> nums) {   
+    var newlist = [...nums,...nums];
+   return newlist;
+  
+  }
+}

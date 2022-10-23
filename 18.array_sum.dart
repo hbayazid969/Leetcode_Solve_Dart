52554785dbf80fc7768c0a10cfaa7629264a8d39
Bayazid Hasan <78470516+hbayazid969@users.noreply.github.com>
2022-10-23 12:54:21 +0600

    Create 18.array_sum.dart

---
+++ 18.array_sum.dart
@@ -0,0 +1,14 @@
+//leetcode
+//https://leetcode.com/problems/running-sum-of-1d-array/
+//1480. Running Sum of 1d Array
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+         for(int i = 1; i < nums.size(); ++i) 
+  {
+    nums[i] = nums[i] + nums[i - 1];
+  }
+   return nums;
+    }
+}
